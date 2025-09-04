bf27ce3087e6a6af9b64d008140e98cfe6897833
Mohd Maroof <146874434+maroofmalik1@users.noreply.github.com>
2025-09-04 11:17:37 +0530

    Update backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000", // backend URL
+  baseURL: "https://task-app-backend-ndgj.onrender.com", // backend URL
 });
 
 // attach token if exists
